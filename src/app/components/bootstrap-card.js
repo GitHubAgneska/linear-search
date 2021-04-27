c4edec23136901100364fe42b1d19d2e2f72c05f
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2021-04-27 05:09:56 +0200

    adding recipeslist data (default ordering)

--- src/app/components/bootstrap-card.js
+++ src/app/components/bootstrap-card.js
@@ -11,12 +11,32 @@ export class CardTemplate extends HTMLElement {
             <div class="card recipe-card col">
                 <img src="" class="card-img-top" alt="">
                 <div class="card-body">
-                    <h5 class="card-title">${recipe.name}</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <div class="card-header-recipe row">
+                        <h5 class="card-title col-8">${recipe.name}</h5>
+                        <div class="card-time col-4 row">
+                            <i class="far fa-clock col"></i>
+                            <h5 class="time col">${recipe.time}min</h5>
+                        </div>
+                    </div>
+                    <div class="recipe-description row">
+                        <ul id="ingredients-list" class="col>
+                        </ul>
+                        <p class="col">${recipe.description}</p>
+                    </div>
                 </div>
             </div>
             `;
+
+            const ingredientsList = this.querySelector('#ingredients-list');
+            const recipeDescr = this.querySelector('#recipe-description');
+
+            recipe.ingredients.forEach(ingredient => {
+                let newListItem = document.createElement('li');
+                let newListItemContent = document.createTextNode(ingredient.ingredient + ':' + ingredient.quantity +  ingredient.unit || null);
+                newListItem.appendChild(newListItemContent);
+                ingredientsList.appendChild(newListItem);
+
+            });
     }
 }
 // register custom element in the built-in CustomElementRegistry object
